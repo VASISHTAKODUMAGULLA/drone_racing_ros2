2722809dda5973d9f7d517dab6c3abcc53f7a601
Vasista <vasishtakodumagulla007@gmail.com>
2025-04-22 01:05:38 +0300

    updates

---
+++ build/tello_driver/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vasista/drone_racing_ros2_ws/tello_ros/tello_driver
+# Build directory: /home/vasista/drone_racing_ros2_ws/build/tello_driver
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/tello_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vasista/drone_racing_ros2_ws/tello_ros/tello_gazebo
+# Build directory: /home/vasista/drone_racing_ros2_ws/build/tello_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
