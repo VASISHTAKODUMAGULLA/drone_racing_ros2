0af503600dece6442385f78ffaf37ecc937ad174
Vasista <vasishtakodumagulla007@gmail.com>
2025-04-23 02:36:48 +0300

    code optimizations and and redundancy removals

--- scripts/drone.py
+++ scripts/drone.py
@@ -1,3 +1,4 @@
+import rclpy
 from random import randint
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -6,11 +7,8 @@ from geometry_msgs.msg import Twist, Point, PoseStamped
 from nav_msgs.msg import Odometry
 from cv_bridge import CvBridge
 
-import rclpy
 import gate_detector as gate_detector
-import _Flight as _Flight
-# import _Utils as _Utils
-import cv2 as cv2
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
@@ -37,7 +35,7 @@ class utility:
         plt.pause(0.001)
 
 
-class Drone(Node, gate_detector.Mixin, _Flight.Mixin, utility):
+class Drone(Node, gate_detector.Detection, gate_detector.Commands, utility):
     def __init__(self, sim=False, *args, **kwargs):
         super().__init__(f'drone_{randint(0, 1000)}')
         qos_policy = rclpy.qos.QoSProfile(reliability=rclpy.qos.ReliabilityPolicy.BEST_EFFORT,
@@ -433,3 +431,53 @@ class Drone(Node, gate_detector.Mixin, _Flight.Mixin, utility):
                 self.searching = True
                 self.moving = False
         return
+
+
+
+
+def main():
+
+
+    # devices = ie.available_devices
+
+    # for device in devices:
+    #     device_name = ie.get_property(device, "FULL_DEVICE_NAME")
+    #     print(f"{device}: {device_name}")
+
+    # classification_model_xml = "openvino_midas_v21_small_256.xml"
+    # model = ie.read_model(model=classification_model_xml)
+    # compiled_model = ie.compile_model(model=model, device_name="CPU")
+    # input_layer = compiled_model.input(0)
+    # output_layer = compiled_model.output(0)
+
+    compiled_model = None
+    input_layer = None
+    output_layer = None
+
+    rclpy.init()
+    sim = True
+    minimal_client = Drone(sim=sim, model=compiled_model, input_layer=input_layer, output_layer=output_layer, gate_color='all')
+
+    try:
+        minimal_client.get_logger().info("Starting node")
+        if sim:
+            minimal_client.send_request_simulator('takeoff')
+        else:
+            minimal_client.take_off()
+        while rclpy.ok():
+            rclpy.spin_once(minimal_client)
+    except KeyboardInterrupt:
+        # Press Ctrl+C to stop the program
+        pass
+    finally:
+        if sim:
+            minimal_client.send_request_simulator('land')
+        else:
+            minimal_client.land()
+        minimal_client.get_logger().info('Shutting down')
+        minimal_client.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
