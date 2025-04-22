153dd4a53eccaf04c311f3101906a7736c14bf5b
Vasista <vasishtakodumagulla007@gmail.com>
2025-04-22 20:39:45 +0300

    Remove build artifacts from repo

--- build/ros2_shared/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vasista/drone_racing_ros2_ws/ros2_shared
-# Build directory: /home/vasista/drone_racing_ros2_ws/build/ros2_shared
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tello_msgs/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vasista/drone_racing_ros2_ws/tello_ros/tello_msgs
-# Build directory: /home/vasista/drone_racing_ros2_ws/build/tello_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tello_msgs__py")
